3d1bca4eb07887514a9865595c94cdc700b01331
voldi-ux <itsvee156@gmail.com>
2024-04-24 17:27:58 +0200

    added firebase

---
+++ src/main/java/com/intermediary/User.java
@@ -0,0 +1,32 @@
+package com.intermediary;
+
+public class User {
+	private String uid;
+	private String username;
+	private String email;
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module com.intermediary {
     requires javafx.web;
 	requires blockchain;
 	requires javafx.graphics;
+	requires firebase.admin;
+	
  
 	
     opens com.intermediary.controllers to javafx.fxml;
